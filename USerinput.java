aa852861ee38b3c1ccbf9a4a0b6d9ee1f842b241
Diwesh rai <77403274+diweshrai@users.noreply.github.com>
2023-06-15 19:20:11 +0530

    User input added

---
+++ USerinput.java
@@ -0,0 +1,28 @@
+package a;
+
+import java.util.Scanner;
+
+
+public class USerinput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.println("Enter Your First Number");
+	int num1 = scan.nextInt();
+
+	System.out.println("Enter Your Second Number");
+	int num2 = scan.nextInt();
+	
+
+	System.out.println(" Your  Number are"+num1+num2);
+	
+		
+		
+		
+		
+	}
+
+}
